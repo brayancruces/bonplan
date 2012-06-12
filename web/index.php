49776929b9f8ac70b03346779bf617dceec44e09
quentin <quentin.pautrat@gmail.com>
2012-06-12 23:17:29 +0200

    Add UrlGeneratorProvider and use path twig function

--- web/index.php
+++ web/index.php
@@ -9,9 +9,10 @@ $app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider(), array()) ;
 
 $app->get('/', function() use ($app){
     return $app['twig']->render('home.twig.html');
-});
+})->bind('home');
 
 $app->run();
