dcf50a9bf8fc2fe1e4ad115d64e058c02c8bfaf5
quentin <quentin.pautrat@gmail.com>
2012-06-12 23:56:39 +0200

    Add the recherche page

--- controllers/bonplan.php
+++ controllers/bonplan.php
@@ -7,4 +7,8 @@ $bonplan->get('/categories', function() use ($app) {
     return $app['twig']->render('bonplan/categories.twig.html');
 })->bind('categories');
 
+$bonplan->get('/recherche', function() use ($app) {
+    return $app['twig']->render('bonplan/recherche.twig.html');
+})->bind('recherche');
+
 return $bonplan;
