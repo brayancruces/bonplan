c84bcb7360067dde03f0d02c2396845d69118cf2
qpautrat <quentin.pautrat@gmail.com>
2012-06-13 23:10:31 +0200

    Add favoris page

--- controllers/bonplan.php
+++ controllers/bonplan.php
@@ -15,4 +15,8 @@ $bonplan->get('/plandujour', function() use ($app) {
     return $app['twig']->render('bonplan/plandujour.twig.html');
 })->bind('plandujour');
 
+$bonplan->get('/favoris', function() use ($app) {
+    return $app['twig']->render('bonplan/favoris.twig.html');
+})->bind('favoris');
+
 return $bonplan;
