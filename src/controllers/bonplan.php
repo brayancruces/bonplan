eae916af5551ea000f0799a8068af8fe5fa0a778
qpautrat <quentin.pautrat@gmail.com>
2012-06-19 02:19:09 +0200

    Change variable name

--- src/controllers/bonplan.php
+++ src/controllers/bonplan.php
@@ -2,21 +2,21 @@
 
 use Silex\ControllerCollection;
 
-$bonplan = $app['controllers_factory'];
+$bonplan_controller = $app['controllers_factory'];
 
-$bonplan->get('/categories', function() use ($app) {
+$bonplan_controller->get('/categories', function() use ($app) {
     return $app['twig']->render('bonplan/categories.twig.html');
 })->bind('categories');
 
-$bonplan->get('/recherche', function() use ($app) {
+$bonplan_controller->get('/recherche', function() use ($app) {
     return $app['twig']->render('bonplan/recherche.twig.html');
 })->bind('recherche');
 
-$bonplan->get('/plandujour', function() use ($app) {
+$bonplan_controller->get('/plandujour', function() use ($app) {
     return $app['twig']->render('bonplan/plandujour.twig.html');
 })->bind('plandujour');
 
-$bonplan->get('/favoris', function() use ($app) {
+$bonplan_controller->get('/favoris', function() use ($app) {
     return $app['twig']->render('bonplan/favoris.twig.html');
 })->bind('favoris');
 
@@ -24,8 +24,8 @@ $bonplan->get('/ajouter', function() use ($app) {
 	return $app['twig']->render('bonplan/ajouter.twig.html');
 })->bind('ajouter');
 
-$bonplan->get('/detail', function() use ($app) {
+$bonplan_controller->get('/detail', function() use ($app) {
 	return $app['twig']->render('bonplan/detail.twig.html');
 })->bind('detail');
 
-return $bonplan;
+return $bonplan_controller;
