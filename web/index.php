2a2e6581187cf1bcf68ea334f814af00cc641ab8
qpautrat <quentin.pautrat@gmail.com>
2012-06-19 02:17:19 +0200

    Add form provider and translation provider

--- web/index.php
+++ web/index.php
@@ -7,14 +7,22 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
+    'twig.path' => __DIR__.'/../src/views',
 ));
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider(), array()) ;
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider(), array());
+
+$app->register(new Silex\Provider\FormServiceProvider());
+
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+      'locale' => 'en',
+      'translator.messages' => array()
+));
 
 $app->get('/', function() use ($app){
     return $app['twig']->render('home.twig.html');
 })->bind('home');
 
-$app->mount('/bonplan', include __DIR__.'/../controllers/bonplan.php');
+$app->mount('/bonplan', include __DIR__.'/../src/controllers/bonplan.php');
 
 $app->run();
