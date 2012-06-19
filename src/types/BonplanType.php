55ebf46eb93c1599f6c1d1b4cd46b4ba0d9cac9e
qpautrat <quentin.pautrat@gmail.com>
2012-06-19 02:20:43 +0200

    Add a new entity and his type

---
+++ src/types/BonplanType.php
@@ -0,0 +1,19 @@
+<?php
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class BonplanType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('date');
+        $builder->add('lieu');
+        $builder->add('description');
+    }
+
+    public function getName()
+    {
+        return 'bonplan';
+    }
+}
