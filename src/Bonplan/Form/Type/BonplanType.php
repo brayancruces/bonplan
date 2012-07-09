cc3a8a35e3d50685d5ee219ad03124b5de11bd3d
qpautrat <qpautrat@interfaces.fr>
2012-07-09 17:37:35 +0200

    Change the widget type

--- src/Bonplan/Form/Type/BonplanType.php
+++ src/Bonplan/Form/Type/BonplanType.php
@@ -4,12 +4,17 @@ namespace Bonplan\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Locale\Stub\StubIntlDateFormatter;
 
 class BonplanType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('date');
+        $builder->add('date', 'date', array(
+            'widget' => 'single_text',
+            'input'  => 'string',
+            'format' => 'yyyy-MM-dd'
+        ));
         $builder->add('lieu');
         $builder->add('description', 'textarea');
     }
